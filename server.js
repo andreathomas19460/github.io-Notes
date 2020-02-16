65d22303f69441e86cd71510a30d645871f95ce9
Andrea Thomas <ANDREA.THOMAS19460@GMAIL.COM>
2020-02-16 10:57:05 -0800

    added server file

---
+++ server.js
@@ -0,0 +1,74 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const uuidv1 = require('uuid/v1');
+
+const app =  express();
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.get('/api/notes', (req, res) => {
+  fs.readFile('db/db.json', 'utf8', function(err, contents) {
+    var words = JSON.parse(contents);
+    res.send(words);
+  });
+});
+
+
+app.post('/api/notes', (req, res) => {
+  fs.readFile('db/db.json',(err, data) => {
+    if (err) throw err;
+    let json = JSON.parse(data);
+    let note = {
+      title: req.body.title,
+      text: req.body.text,
+      id: uuidv1()
+    }
+  
+    json.push(note);
+
+    fs.writeFile('db/db.json', JSON.stringify(json, null, 2), (err) => {
+      if (err) throw err;
+      res.send('200');
+    });
+  });
+});
+
+
+app.delete('/api/notes/:id', (req, res) => {
+
+  fs.readFile('db/db.json',(err, data) => {
+    if (err) throw err;
+    let deleteId = req.params.id;
+    let json = JSON.parse(data);
+    json.forEach((item, i) =>{
+      if (item.id.includes(deleteId)){ 
+        json.splice(i, 1);       
+      }
+    });
+
+    fs.writeFile('db/db.json', JSON.stringify(json, null, 2), (err) => {
+      if (err) throw err;
+      res.send('200');
+    });
+  });
+
+})
+
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'home', 'index.html'));
+});
+
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, 'home', 'note.html'));
+});
+
+app.use(express.static(path.join(__dirname, 'home')));
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT);
